b956a8d4dc216183f3c847a4abbd7ebab69e396f
Getnett <getnetendaylalu2015@gmail.com>
2024-09-17 16:40:23 +0300

    Add CI for node.js

--- dist/server.js
+++ dist/server.js
@@ -1,22 +1,24 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 3000;
 app.get("/", (req, res) => {
-    // Send a response to the client
-    res.send(`<html>
+  // Send a response to the client
+  res.send(`<html>
          <head>
            <title>Personal finance managment project</title>
          </head>
          <body>
-          <h1>Personal finance managment app updated!</h1>
+          <h1>Personal finance managment app updated! another update!</h1>
          </body>
     </html>`);
 });
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
