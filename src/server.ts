fb42d45d4ef474f1c1670add698e45fe085a2f1b
Getnett <getnetendaylalu2015@gmail.com>
2024-09-17 15:45:42 +0300

    git the workflow working

--- src/server.ts
+++ src/server.ts
@@ -16,8 +16,6 @@ app.get("/", (req, res) => {
     </html>`);
 });
 
-// Start the server and listen on the specified port
 app.listen(port, () => {
-  // Log a message when the server is successfully running
   console.log(`Server is running on http://localhost:${port}`);
 });
