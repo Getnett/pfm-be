91b47b7c9d75366c104b4e89281884513a9ae412
Getnett <getnetendaylalu2015@gmail.com>
2024-09-18 13:07:52 +0300

    fix pre-commit husky

--- dist/server.js
+++ dist/server.js
@@ -13,7 +13,7 @@ app.get("/", (req, res) => {
            <title>Personal finance managment project</title>
          </head>
          <body>
-          <h1>Personal finance managment app updated! as a new project!</h1>
+          <h1>Personal finance managment app updated! almost ready for dev :)</h1>
          </body>
     </html>`);
 });
