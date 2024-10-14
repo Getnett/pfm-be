7577c946e5137b1f9bd4089cce7a7135ca67b510
Getnett <getnetendaylalu2015@gmail.com>
2024-10-14 20:33:23 +0300

    Update expenses schema

---
+++ migrations/1728927024838_update-expenses-table-date-field.js
@@ -0,0 +1,29 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.sql(`
+    ALTER TABLE expenses
+    ALTER COLUMN date SET NOT NULL;
+    
+    `);
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.sql(`
+    ALTER TABLE expenses
+    ALTER COLUMN date DROP NOT NULL;
+    `);
+};
