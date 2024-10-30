8b502cb591ae1e80ee34aaf0d7fac03b8b9c6dfc
Getnett <getnetendaylalu2015@gmail.com>
2024-10-30 14:00:08 +0300

    fix a migration bug due to removing data

---
+++ migrations/1730285969339_init-table-users-second-time.js
@@ -0,0 +1,20 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.sql(`
+       INSERT INTO users (username) 
+       VALUES('default'); 
+        `);
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.sql(`DELETE FROM users WHERE username = 'default'; `);
+};
