c232a95bd629ea8233744f106d4dd41e2aeed9a3
Getnett <getnetendaylalu2015@gmail.com>
2024-10-30 13:41:51 +0300

    add default account

---
+++ migrations/1730283477932_default-account-data.js
@@ -0,0 +1,23 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.sql(`INSERT INTO accounts (account_name,balance,note,user_id,account_type)
+  VALUES("default",0,"default account",1,1)`);
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.sql(`DELETE FROM accounts WHERE account_type = 1`);
+};
