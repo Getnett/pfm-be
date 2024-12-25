5a100974c66bf32a059aeef0350930080964a007
Getnett <getnetendaylalu2015@gmail.com>
2024-12-25 16:09:09 +0300

    small for fix for the transactions api

--- src/repos/transactions.ts
+++ src/repos/transactions.ts
@@ -4,34 +4,35 @@ import toCamelCase from "../utils/to-camel-case";
 export default class TransactionsRepo {
   static async getTransactions(month: number, year: number) {
     const { rows } = await dbPool.query(
-      ` WITH expense_transaction  AS (SELECT 
-        DATE(date) AS transaction_date,
+      `WITH expense_transaction  AS (SELECT 
+        TO_CHAR(expenses.date, 'Mon DD Day') AS transaction_date,
         SUM(amount) AS total,
         'expense' AS type
         FROM expenses 
         WHERE EXTRACT(YEAR FROM date) = $2 AND EXTRACT(MONTH FROM date) = $1
-        GROUP BY DATE(date)
+        GROUP BY TO_CHAR(expenses.date,'Mon DD Day')
         ),
-        income_transaction  AS (
+         income_transaction  AS (
         SELECT 
-        DATE(date) AS transaction_date,
+        TO_CHAR(incomes.date, 'Mon DD Day')  AS transaction_date,
         SUM(amount) AS total,
         'income' AS type
         FROM incomes
         WHERE EXTRACT(YEAR FROM date) = $2 AND EXTRACT(MONTH FROM date) = $1
-        GROUP BY DATE(date)
+        GROUP BY TO_CHAR(incomes.date,'Mon DD Day')
         ),
         exp_trans AS (
-          SELECT id,amount,note,date,transaction_date,total,type FROM expenses
-          JOIN expense_transaction ON  DATE(expenses.date) = expense_transaction.transaction_date
-          ),
+        SELECT id,amount,note,date,transaction_date,total,type FROM expenses
+        JOIN expense_transaction ON TO_CHAR(expenses.date, 'Mon DD Day')  = expense_transaction.transaction_date
+        ),
         income_trans AS (
-          SELECT id,amount,note,date,transaction_date,total,type FROM incomes
-           JOIN income_transaction ON DATE(incomes.date) = income_transaction.transaction_date
-          ) 
-         SELECT * FROM exp_trans
-         UNION ALL 
-         SELECT * FROM income_trans;
+        SELECT id,amount,note,date,transaction_date,total,type FROM incomes
+         JOIN income_transaction ON TO_CHAR(incomes.date, 'Mon DD Day')  = income_transaction.transaction_date
+        ) 
+        
+        SELECT * FROM exp_trans
+        UNION ALL 
+        SELECT * FROM income_trans;
         `,
       [month, year]
     );
